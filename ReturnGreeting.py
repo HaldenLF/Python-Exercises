da7ee79311959791567e8d71cd6760f29cd1a9b3
HaldenLF <Liamhalliden20@gmail.com>
2024-05-15 15:21:33 +0700

    Added exercise

---
+++ ReturnGreeting.py
@@ -0,0 +1,11 @@
+# Exercise 2
+# ask for user name through input and return that with greeting
+
+def greeting():
+    print("Hello, what is your name")
+
+    userInput = input('> \n')
+
+    print("Hello, " + userInput + ". Nice to meet you!")
+
+greeting()
