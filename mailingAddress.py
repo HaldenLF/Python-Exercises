a4191eb97e2e1c2e2edf74edbca91c4ce4e15baf
HaldenLF <Liamhalliden20@gmail.com>
2024-05-15 15:14:23 +0700

    Added mailing address exercise

---
+++ mailingAddress.py
@@ -0,0 +1,13 @@
+# Exercise 1
+# Print out name and address like you would see on an envelope
+
+def mailingAddress():
+    output = ("Full name,\n"
+              "1st address line,\n"
+              "2nd address line,\n"
+              "City,\n"
+              "State,\n" 
+              "Zip code")
+    print(output)
+
+mailingAddress()
